1888535e5456eed0f4f6d0a2145d5279dcaec9e3
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-13 04:30:44 +0300

    add comment icon

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -13,7 +13,7 @@ import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AddCommentIcon from '@mui/icons-material/AddComment';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -44,13 +44,7 @@ function Post(props) {
           R
         </Avatar>
       }
-      action={
-        <IconButton aria-label="settings">
-          <MoreVertIcon />
-        </IconButton>
-      }
       title={title}
-      subheader="September 14, 2016"
     />
 
     <CardContent>
@@ -62,16 +56,13 @@ function Post(props) {
       <IconButton aria-label="add to favorites">
         <FavoriteIcon />
       </IconButton>
-      <IconButton aria-label="share">
-        <ShareIcon />
-      </IconButton>
       <ExpandMore
         expand={expanded}
         onClick={handleExpandClick}
         aria-expanded={expanded}
         aria-label="show more"
       >
-        <ExpandMoreIcon />
+        <AddCommentIcon />
       </ExpandMore>
     </CardActions>
     <Collapse in={expanded} timeout="auto" unmountOnExit>
