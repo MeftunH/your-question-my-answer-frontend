82b0357614b82717f686c3684c1b5823681ab26b
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-22 05:59:15 +0300

    format comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -4,13 +4,13 @@ import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import { red } from "@mui/material/colors";
 import {
-    Button,
-    InputAdornment,
-    OutlinedInput,
-    Snackbar,
-    Alert,
-    Link
-  } from "@mui/material";
+  Button,
+  InputAdornment,
+  OutlinedInput,
+  Snackbar,
+  Alert,
+  Link,
+} from "@mui/material";
 
 function Comment(props) {
   const { text, userId, userName } = props;
@@ -30,19 +30,19 @@ function Comment(props) {
           value={text}
           startAdornment={
             <InputAdornment position="start">
-               <Link
-              style={{
-                textDecoration: "none",
-                boxShadow: "none",
-                color: "grey",
-              }}
-              className="navbar-link"
-              to={{ pathname: "/users/" + userId }}
-            >
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                {userName.charAt(0).toUpperCase()}
-              </Avatar>
-            </Link>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  boxShadow: "none",
+                  color: "grey",
+                }}
+                className="navbar-link"
+                to={{ pathname: "/users/" + userId }}
+              >
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {userName.charAt(0).toUpperCase()}
+                </Avatar>
+              </Link>
             </InputAdornment>
           }
         ></OutlinedInput>
