9dd2ff7a92cf0d0b79da0cfa3c88e50f055643cd
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-30 00:00:30 +0300

    set margin

--- src/components/authentication/Auth.js
+++ src/components/authentication/Auth.js
@@ -1,18 +1,15 @@
-import { FormControl,Input,InputLabel } from '@mui/material'
-import React from 'react'
+import { FormControl, Input, InputLabel } from "@mui/material";
+import React from "react";
 
 function Auth() {
   return (
-    <FormControl>
-        <InputLabel>
-        Username
-        </InputLabel>
-        <Input></Input>
-        <InputLabel>
-        Password
-        </InputLabel>
-        </FormControl>
-  )
+    <FormControl size="large">
+      <InputLabel>Username</InputLabel>
+      <Input></Input>
+      <InputLabel style={{ top: 30 }}>Password</InputLabel>
+      <Input style={{ top: 30 }}></Input>
+    </FormControl>
+  );
 }
 
-export default Auth
+export default Auth;
